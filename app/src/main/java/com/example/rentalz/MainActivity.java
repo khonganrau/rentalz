06e8776cf1241c9693063c92cefdaa7bb8ac5d43
khonganrau <quynh.dm1411@gmail.com>
2021-09-12 00:48:52 +0700

    update

--- app/src/main/java/com/example/rentalz/MainActivity.java
+++ app/src/main/java/com/example/rentalz/MainActivity.java
@@ -13,17 +13,24 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 
+import com.google.android.material.textfield.MaterialAutoCompleteTextView;
 import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-
+    private MaterialAutoCompleteTextView auto_txt_bedrooms;
+    ArrayList<String> bedrooms_arrList;
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner spinner = findViewById(R.id.spinerProperty);
+        mapping();
+
+        Spinner spinner = findViewById(R.id.spinner_property);
 
         ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.property,
                 android.R.layout.simple_spinner_item);
@@ -31,7 +38,20 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         ;
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+
+        adapter = new ArrayAdapter<String>(getApplicationContext(),R.layout.support_simple_spinner_dropdown_item,bedrooms_arrList);
+        auto_txt_bedrooms.setAdapter(adapter);
+        auto_txt_bedrooms.setThreshold(1);
     }
+
+    private void mapping() {
+        auto_txt_bedrooms = findViewById(R.id.ac_txt_bedroom);
+        bedrooms_arrList = new ArrayList<>();
+        for(int i = 1; i <= 30 ; i++){
+            bedrooms_arrList.add(i +"");
+        }
+    }
+
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int postion, long l) {
         String text = parent.getItemAtPosition(postion).toString();
